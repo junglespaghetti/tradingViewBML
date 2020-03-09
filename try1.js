5084cc5a7ba4ceed83a9d43946094e4354e2ec1e
Glitch (kindly-band-heather) <none>
2020-03-09 03:42:42 +0000

    💷🐪 Updated with Glitch

--- try1.js
+++ try1.js
@@ -1,19 +1,16 @@
-// github  user/repo >>> junglespaghetti/tradingViewBML
-// https://script.google.com/macros/s/AKfycbx5aUrcK83KHx6H_XvweA6wTDwFPumMvUXOfxdQ9jkwb5mNy0g/exec
-
 javascript: (function() {
-   var url = "https://script.google.com/macros/s/AKfycbx5aUrcK83KHx6H_XvweA6wTDwFPumMvUXOfxdQ9jkwb5mNy0g/exec";
-   var xhr = new XMLHttpRequest();
-  var data = {"test":"word"};
-   xhr.open("POST", url);
-   xhr.setRequestHeader("Content-Type", "application/json");
-   xhr.onload = () => {
-     alert(xhr.status);
-     alert("success!");
-   };
-   xhr.onerror = () => {
-     alert(xhr.status);
-     alert("error!");
-   };
-   xhr.send(JSON.stringify(data));
-})()
+  var xhr = new XMLHttpRequest();
+  var url = "";
+  var data = { aaa: "sss" };
+  xhr.open("POST", url);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.onload = () => {
+    //  console.log(xhr.status);
+    alert("success!");
+  };
+  xhr.onerror = () => {
+    //  console.log(xhr.status);
+    alert("error!");
+  };
+  xhr.send(JSON.stringify(data));
+})();
